ab2946cb7b95ffb30eff45b372a63bdde9504101
Wealon11 <47988706+Wealon11@users.noreply.github.com>
2019-12-08 17:21:36 +0100

    created asp.net core project
    first

---
+++ MicroService/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MicroService.Models;
+
+namespace MicroService.Controllers
+{
+    public class HomeController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
